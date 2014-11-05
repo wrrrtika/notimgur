b68857ba46fdf62b63e694f645a528724fbabdc8
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-11-05 18:09:56 -0500

    each picture now appears at its own dynamic URL, like a boss.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,6 +18,29 @@ class PicturesController < ApplicationController
       }
     ]
 
+
+
 	end
+
+	def show
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
+  end
 
 end
