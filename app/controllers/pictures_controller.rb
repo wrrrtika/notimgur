5acce1dea72084ddcfeff04975e75619547a3b1c
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-11-05 19:45:12 -0500

    refactored for optimum database saveage

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,55 +1,29 @@
 class PicturesController < ApplicationController
 
-	def index
-		@pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-      }
-    ]
-
-
-
-	end
-
-	def show
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-      }
-    ]
-    @picture = @pictures[params[:id].to_i]
+  def index
+    @pictures = Picture.all
   end
 
-	def new
-	end
+  def show
+    @picture = Picture.find(params[:id])
+  end
 
-	def create
-		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-	end
+  def new
+    @picture = Picture.new
+  end
 
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_url
+    else
+      render :new
+  end
+end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
 
 end
